57b6b57c21baa1ba69093ac8db2e509f9373f425
Gloria Azum <azumgloria@gmail.com>
2024-06-10 18:14:13 +0100

    updating

--- src/lib.rs
+++ src/lib.rs
@@ -5,39 +5,33 @@ pub struct Triangle {
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
         /* unimplemented!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");*/
-        match sides {
-            [side_one, side_two, side_three] if [side_one, side_two, side_three].iter().all(|&side| side > 0) &&
-                side_one + side_two > side_three &&
-                side_two + side_three > side_one &&
-                side_one + side_three > side_two => {
-                Some(Triangle { sides })
-            }
-            _ => None,
+        let [side_one, side_two, side_three] = sides;
+        if side_one > 0 && side_two > 0 && side_three > 0 &&
+            side_one + side_two > side_three &&
+            side_two + side_three > side_one &&
+            side_one + side_three > side_two {
+            Some(Triangle { sides })
+        } else {
+            None
         }
     }
 
     pub fn is_equilateral(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is equilateral."); */
-        match &self.sides {
-            [side_one, side_two, side_three] if [side_one, side_two, side_three].iter().all(|&side| side > &0) &&
-                side_one == side_two && side_two == side_three => true,
-            _ => false
-        }
+        let [side_one, side_two, side_three] = self.sides;
+        self.sides.iter().all(|&side| side > 0) && side_one == side_two && side_two == side_three
+
     }
 
     pub fn is_scalene(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is scalene."); */
-        match &self.sides {
-            [side_one, side_two, side_three] if side_one != side_two && side_two != side_three && side_one != side_three => true,
-            _ => false
-        }
+        let [side_one, side_two, side_three] = self.sides;
+        side_one != side_two && side_two != side_three && side_one != side_three
     }
 
     pub fn is_isosceles(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is isosceles."); */
-        match &self.sides {
-            [side_one, side_two, side_three] if side_one == side_two || side_one == side_three || side_two == side_three => true,
-            _ => false
-        }
+        let [side_one, side_two, side_three] = self.sides;
+        side_one == side_two || side_one == side_three || side_two == side_three
     }
 }
