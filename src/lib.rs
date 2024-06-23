6c1356a5ad1f7ad8d996f5b9b8425704ed750b5a
Gloria Azum <azumgloria@gmail.com>
2024-06-23 01:41:27 +0100

    updating

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ pub struct Triangle {
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
         /* unimplemented!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");*/
-        let [side_one, side_two, side_three] = sides;
-        if side_one > 0 && side_two > 0 && side_three > 0 &&
-            side_one + side_two > side_three &&
-            side_two + side_three > side_one &&
-            side_one + side_three > side_two {
+        let [a, b, c] = sides;
+        if a > 0 && b > 0 && c > 0 &&
+            a + b > c &&
+            b + c > a &&
+            a + c > b {
             Some(Triangle { sides })
         } else {
             None
@@ -18,20 +18,20 @@ impl Triangle {
 
     pub fn is_equilateral(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is equilateral."); */
-        let [side_one, side_two, side_three] = self.sides;
-        self.sides.iter().all(|&side| side > 0) && side_one == side_two && side_two == side_three
+        let [a, b, c] = self.sides;
+        self.sides.iter().all(|&side| side > 0) && a == b && b == c
 
     }
 
     pub fn is_scalene(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is scalene."); */
-        let [side_one, side_two, side_three] = self.sides;
-        side_one != side_two && side_two != side_three && side_one != side_three
+        let [a, b, c] = self.sides;
+        a != b && b != c && a != c
     }
 
     pub fn is_isosceles(&self) -> bool {
         /* unimplemented!("Determine if the Triangle is isosceles."); */
-        let [side_one, side_two, side_three] = self.sides;
-        side_one == side_two || side_one == side_three || side_two == side_three
+        let [a, b, c] = self.sides;
+        a == b || a == c || b == c
     }
 }
